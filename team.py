ba6dd09f94fa4aed23ac794f9e2cc2ff6baf6aee
Mohammed Musharraf <86351406+MohammedMusharraf11@users.noreply.github.com>
2024-08-26 17:13:19 +0530

    Update team.py

--- team.py
+++ team.py
@@ -6,32 +6,11 @@ def team_details():
 
     team_members = [
         {
-            "name": "Mohammadi Shifa",
-            "role": "Lead Developer",
-            "description": "Shifa is passionate about building innovative solutions and has a strong background in web development and AI.",
-            "gmail": "shifamohammadi07@gmail.com",
-            "usn": "USN: 1DT23CA025"
-        },
-        {
-            "name": "Harsha",
-            "role": "Data Scientist",
-            "description": "Harsha specializes in machine learning algorithms and data analysis. She enjoys tackling complex problems.",
-            "gmail": "harshasingh@gmail.com",
-            "usn": "USN: B2345678"
-        },
-        {
-            "name": "Kishitj",
-            "role": "UI/UX Designer",
-            "description": "Kishitj brings creativity to our team with a focus on user experience and interface design.",
-            "gmail": "kishitj@gmail.com",
-            "usn": "USN: C3456789"
-        },
-        {
-            "name": "Fiona Ann Jacob",
-            "role": "Data Scientist",
-            "description": "Fiona specializes in machine learning algorithms and data analysis. She enjoys tackling complex problems.",
-            "gmail": "fionaannjacob@gmail.com",
-            "usn": "USN: 1DT23EC024"
+            "name": "Mohammed Musharraf",
+            "role": "Founder & Developer",
+            "description": "Mohammed Musharraf is dedicated to crafting innovative digital solutions. With a strong foundation in app and web development, he is passionate about utilizing technology to address real-world problems.",
+            "gmail": "iammusharraf11@gmail.com",
+            "usn": "SRN: PES2UG23CS915"
         }
     ]
 
